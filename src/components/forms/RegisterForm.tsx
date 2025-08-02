aa3ff150aeff837d0a5528254536512b5cb7bd01
tranngocphat13 <tranngocphat1305@gmail.com>
2025-08-02 16:15:12 +0700

    2/8 16h15

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { useState } from "react";
 
 export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("Vui lòng điền đầy đủ thông tin");
+      return;
+    }
+
+  };
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
@@ -9,16 +27,31 @@ export default function RegisterForm() {
           Đăng kí tài khoản
         </h1>
 
-        <form className="flex flex-col gap-4">
-          <input type="text" placeholder="Họ và tên" />
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Mật khẩu" />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Họ và tên"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Mật khẩu"
+          />
           <button className="bg-green-500 text-white py-2 px-4 rounded cursor-pointer font-bold">
             Đăng kí
           </button>
-          <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-            Error message
-          </div>
+
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
 
           <Link href="/" className="text-sm text-blue-500 text-right">
             Đã có sẵn tài khoản <span className="underline">Đăng nhập</span>
