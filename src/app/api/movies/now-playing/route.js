ac6b3993b05a1f39f1b3169ef2eb5d2b8199a8e1
tranngocphat13 <tranngocphat1305@gmail.com>
2025-10-02 22:01:09 +0700

    final-build

--- src/app/api/movies/now-playing/route.js
+++ src/app/api/movies/now-playing/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/lib/mongodb";
+import  connectDB from "@/lib/mongodb";
 import Movie from "@/models/movies";
 
 export const dynamic = "force-dynamic";
--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,23 +1,17 @@
-// lib/mongodb.js
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error("Please add MONGODB_URI to .env");
-}
+if (!MONGODB_URI) throw new Error("Please add MONGODB_URI to .env");
 
 let isConnected = false;
 
-export async function connectDB() {
+export default async function connectDB() {
   if (isConnected) return mongoose;
 
   try {
-    const conn = await mongoose.connect(MONGODB_URI, {
-      dbName: "test", // đổi theo tên database của bạn
-    });
+    const conn = await mongoose.connect(MONGODB_URI, { dbName: "test" });
     isConnected = true;
-    console.log("MongoDB connected");
+    console.log("✅ MongoDB connected");
     return conn;
   } catch (err) {
     console.error("MongoDB connection error:", err);
